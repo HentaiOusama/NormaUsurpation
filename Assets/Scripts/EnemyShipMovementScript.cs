24234e707f42c14445627c470edeb3862f6ad4e9
Hentai Ousama <kutabare.eerito@gmail.com>
2020-06-16 11:59:34 +0530

    Built wave 1. This will act as offical wave but a sample. Problems with enemyShip Lighting discovered. Needs to fixed.

--- Assets/Scripts/EnemyShipMovementScript.cs
+++ Assets/Scripts/EnemyShipMovementScript.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using PathCreation;
 using UnityEngine;
 
 public class EnemyShipMovementScript : MonoBehaviour
 {
+    /*Details :-
+    * 
+    * Type = 1 => A to B Introduction and Oscillate at given MovementPath
+    */
+
+
+
+
+    // Non-Serialized Variables
+    bool shouldMoveEnemies = false;
+    bool shouldIntroduce = false;
+    int movementType;
+    PathCreator introductoryPath;
+    PathCreator[] movementPaths;
+    float introSpeed;
+    float movementSpeed;
+    float distanceTravelled = 0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,48 @@ public class EnemyShipMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(shouldMoveEnemies) {
+            switch (movementType) {
+                case 1:
+                    moveType1();
+                    break;
+
+            }
+        }
+    }
+
+    // Fetches data for moving enemies
+    public void startMovingEnemies(int movementType, GameObject introductoryPath, GameObject[] movementPaths, float introSpeed, float movementSpeed) {
+
+        this.introductoryPath = introductoryPath.GetComponent<PathCreator>();
+        this.movementPaths = new PathCreator[movementPaths.Length];
+        for(int i = 0; i < movementPaths.Length; i++) {
+            this.movementPaths[i] = movementPaths[i].GetComponent<PathCreator>();
+        }
+        this.introSpeed = introSpeed;
+        this.movementSpeed = movementSpeed;
+        this.movementType = movementType;
+        shouldIntroduce = true;
+        shouldMoveEnemies = true;
+    }
+
+
+    // Different Movement Types
+    void moveType1() {
+        if(shouldIntroduce) {
+            distanceTravelled += introSpeed * Time.deltaTime;
+            gameObject.transform.position = introductoryPath.path.GetPointAtDistance(distanceTravelled);
+            if(distanceTravelled >= introductoryPath.path.length) {
+                distanceTravelled = introductoryPath.path.length;
+                gameObject.transform.position = introductoryPath.path.GetPointAtDistance(distanceTravelled);
+                shouldIntroduce = false;
+                distanceTravelled = 0;
+            }
+            return;
+        }
+ 
+        distanceTravelled += movementSpeed * Time.deltaTime;
+        gameObject.transform.position = movementPaths[0].path.GetPointAtDistance(distanceTravelled);
         
     }
 }
--- Assets/Scripts/EnemyWaveBuilder.cs
+++ Assets/Scripts/EnemyWaveBuilder.cs
@@ -42,7 +42,7 @@ public class EnemyWaveBuilder : MonoBehaviour
             shouldBuildEnemyWave = false;
             switch(waveBuildingData.waveLevel) {
                 case 1:
-                    buildWave1(0, 1, 5);
+                    buildWave1(0, 0, 4, 6, viewableScreenConstrains.top - 4f, 1);
                     break;
             }
         }
@@ -56,32 +56,55 @@ public class EnemyWaveBuilder : MonoBehaviour
 
 
     // Not fully complete
-    public void buildWave1(int index, int numberOfLayers, int enemiesPerLayer){
+    public void buildWave1(int enemyShipIndex,int numberOfLayersToSkip, int numberOfLayersToBuild, int enemiesPerLayer, float startUpperHeight, float verticalGapSize){
         // index => which enemyShip to build
-
-        float screenWidth = 2*viewableScreenConstrains.right;
+        float screenWidth, enemyShipWidth, enemyShipHeight, horizontalGapSize, startPosLeft, startPosTop = startUpperHeight;
+        screenWidth = 2*viewableScreenConstrains.right;
+        Vector3 centerPosition;
+        GameObject tempGameObject, multipleEnemyHolder;
+        SizeData tempSizeData;
         
-        GameObject tempObj = Instantiate(waveBuildingData.enemyObjects[index], new Vector3(0, 25, 0), Quaternion.Euler(0, 0, 0)) as GameObject; // Temporary Instantiation
-        SizeData enemyShipSizeData = tempObj.GetComponent<SizeData>();
+        // Get data regarding the ship we are going to build by instantiating a temporary enemyShip to get SizeData and then delete it.
+        tempGameObject = Instantiate(waveBuildingData.enemyObjects[enemyShipIndex], new Vector3(0, 25, 0), Quaternion.Euler(0, 0, 0)) as GameObject; // Temporary Instantiation
+        tempSizeData = tempGameObject.GetComponent<SizeData>();
+        enemyShipWidth = (tempSizeData.occupiedDistance.x/tempSizeData.defaultScaleForUse.x)*tempGameObject.transform.localScale.x;
+        enemyShipHeight = (tempSizeData.occupiedDistance.z/tempSizeData.referenceScale.z)*tempGameObject.transform.localScale.z;
+        Destroy(tempGameObject);
+        horizontalGapSize = (screenWidth/enemiesPerLayer) - enemyShipWidth;
+        startPosTop -= numberOfLayersToSkip*(enemyShipHeight);
+        centerPosition = new Vector3(2*viewableScreenConstrains.right, 0, startPosTop - ((enemyShipHeight/2) * numberOfLayersToBuild));
+        multipleEnemyHolder = Instantiate(waveBuildingData.multipleEnemyHolder, new Vector3(centerPosition.x, 0, centerPosition.z), 
+                                        Quaternion.Euler(0, 0, 0)) as GameObject;
 
-        float enemyShipWidth = (enemyShipSizeData.occupiedDistance.x/enemyShipSizeData.defaultScaleForUse.x)*tempObj.transform.localScale.x;
-        float gapSize = (screenWidth/enemiesPerLayer) - enemyShipWidth;
 
-        GameObject multipleEnemyHolder = Instantiate(waveBuildingData.multipleEnemyHolder, new Vector3(2*viewableScreenConstrains.right, 0, 0), 
-                                                    Quaternion.Euler(0, 0, 0)) as GameObject;
-        Destroy(tempObj);
-        float startPos = viewableScreenConstrains.right;
-        for(int i = 0; i < enemiesPerLayer; i++) {
-            startPos += gapSize/2;
-            startPos += enemyShipWidth/2;
-            tempObj = Instantiate(waveBuildingData.enemyObjects[index], new Vector3(startPos, 0, 0), Quaternion.Euler(0, 0, 0)) as GameObject;
-            tempObj.transform.SetParent(multipleEnemyHolder.transform);
-            startPos += enemyShipWidth/2;
-            startPos += gapSize/2;
-            totalEnemyCount++;
+        for(int i = 0; i < numberOfLayersToBuild; i++) {
+            startPosTop -= verticalGapSize/2;
+            startPosTop -= enemyShipHeight/2;
+            startPosLeft = viewableScreenConstrains.right;
+            for(int j = 0; j < enemiesPerLayer; j++) {
+                startPosLeft += horizontalGapSize/2;
+                startPosLeft += enemyShipWidth/2;
+                tempGameObject = Instantiate(waveBuildingData.enemyObjects[enemyShipIndex], new Vector3(startPosLeft, 0, startPosTop),
+                                            Quaternion.Euler(0, 0, 0)) as GameObject;
+                tempGameObject.transform.SetParent(multipleEnemyHolder.transform);
+                startPosLeft += enemyShipWidth/2;
+                startPosLeft += horizontalGapSize/2;
+                totalEnemyCount++;
+            }
+            startPosTop -= enemyShipHeight/2;
+            startPosTop -= verticalGapSize/2;
         }
 
-        tempObj = Instantiate(waveBuildingData.movementPathObjects[1], new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0)) as GameObject;
+        tempGameObject = Instantiate(waveBuildingData.movementPathObjects[1], new Vector3(0, 0, centerPosition.z), 
+                                    Quaternion.Euler(0, 0, 0)) as GameObject;
+        tempSizeData = tempGameObject.GetComponent<SizeData>();
+        tempGameObject.transform.localScale = tempSizeData.referenceScale * (horizontalGapSize/tempSizeData.occupiedDistance.x);
+        GameObject introPath = Instantiate(waveBuildingData.movementPathObjects[0], new Vector3(centerPosition.x/2, 0, centerPosition.z), 
+                                            Quaternion.Euler(0, 180, 0)) as GameObject;
+        tempSizeData = introPath.GetComponent<SizeData>();
+        introPath.transform.localScale = tempSizeData.referenceScale * ((multipleEnemyHolder.transform.position.x - 
+                                            tempGameObject.transform.position.x) / tempSizeData.occupiedDistance.x);
+        multipleEnemyHolder.AddComponent<EnemyShipMovementScript>().startMovingEnemies(1, introPath, new GameObject[]{tempGameObject}, 2, 4);
     }
 
 
