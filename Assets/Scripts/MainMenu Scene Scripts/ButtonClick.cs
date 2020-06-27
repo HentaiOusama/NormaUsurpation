6d26e5f5640cd0fc787a5e7e751178d35e0b37fc
Hentai Ousama <kutabare.eerito@gmail.com>
2020-06-28 01:26:06 +0530

    In previous commit, SFX were also added. Forgot to mention... TEHE... In this commit, Button animations are added when clicked

--- Assets/Scripts/MainMenu Scene Scripts/ButtonClick.cs
+++ Assets/Scripts/MainMenu Scene Scripts/ButtonClick.cs
@@ -1,8 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonClick : MonoBehaviour
 {
+    public int buttonId;
+
+    private Animator menuButtonAnimator;
+    bool shouldReset = false;
+
+    void Start() {
+        menuButtonAnimator = gameObject.GetComponent<Animator>();
+        menuButtonAnimator.ResetTrigger("didClick");
+        menuButtonAnimator.ResetTrigger("goToIdle");
+    }
+
+    void Update() {
+        if(shouldReset) {
+            if(menuButtonAnimator.GetCurrentAnimatorStateInfo(0).IsName("MenuButtonIdle")) {
+                menuButtonAnimator.ResetTrigger("didClick");
+                menuButtonAnimator.ResetTrigger("goToIdle");
+                shouldReset = false;
+            }
+        }
+
+        if(menuButtonAnimator.GetCurrentAnimatorStateInfo(0).IsName("MenuButtonAnimationEnded")) {
+            menuButtonAnimator.SetTrigger("goToIdle");
+            menuButtonAnimator.ResetTrigger("didClick");
+            shouldReset = true;
+            
+            switch(buttonId) {
+                
+                case 0:
+                    PlayGame();
+                    break;
+            }
+        }
+    }
+
+    public void buttonClicked() {
+        menuButtonAnimator.SetTrigger("didClick");
+    }
+
     public void PlayGame() {
         SceneManager.LoadScene(1);
     }
